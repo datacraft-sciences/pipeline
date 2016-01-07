34e187424a0c4f2d995246be349a2c32dffef7b5
mikera <mike.r.anderson.13@gmail.com>
2016-01-07 10:57:54 +0800

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject datacraft/pipeline "0.1.0-SNAPSHOT"
+  :description "Data processing pipeline tools for Clojure"
+  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
+                 [net.mikera/core.matrix "0.48.0"]
+                 [org.clojure/test.check "0.9.0"]]
+
+  :jvm-opts  ["-Xmx8g"])
