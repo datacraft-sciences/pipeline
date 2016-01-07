1e5f240dea53edba788408e53ccf2c54ed567b49
mikera <mike.r.anderson.13@gmail.com>
2016-01-07 11:04:25 +0800

    add cljunit testing, extra deps

--- project.clj
+++ project.clj
@@ -2,6 +2,16 @@
   :description "Data processing pipeline tools for Clojure"
   :dependencies [[org.clojure/clojure "1.8.0-RC4"]
                  [net.mikera/core.matrix "0.48.0"]
-                 [org.clojure/test.check "0.9.0"]]
+                 [org.clojure/test.check "0.9.0"]
+                 [org.clojure/data.csv "0.1.3"]]
+  
+  :source-paths ["src/main/clj"]
+  :test-paths ["src/test/clj"]
+  
+  :resource-paths ["resources"]
+  
+  :profiles {:dev {:dependencies [[net.mikera/cljunit "0.3.1"]]
+                   :java-source-paths ["src/test/java"]}}
+
 
   :jvm-opts  ["-Xmx8g"])
---
+++ src/test/java/datacraft/pipeline/TestClojure.java
@@ -0,0 +1,10 @@
+package datacraft.pipeline;
+
+import mikera.cljunit.ClojureTest;
+
+public class TestClojure extends ClojureTest {
+	@Override
+	public String filter() {
+		return "datacraft.pipeline";
+	}
+}
