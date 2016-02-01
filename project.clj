76e1241ec798292db3c4b1fe13f9768fa39a392a
mikera <mike.r.anderson.13@gmail.com>
2016-02-01 11:20:58 +0800

    Update deps

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject datacraft/pipeline "0.1.0-SNAPSHOT"
   :description "Data processing pipeline tools for Clojure"
-  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
-                 [net.mikera/core.matrix "0.48.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [net.mikera/core.matrix "0.49.0"]
                  [org.clojure/test.check "0.9.0"]
                  [org.clojure/data.csv "0.1.3"]
                  [prismatic/schema "1.0.4"]]
